e0403b3ac07ef85a505e8f63f7403ba2e1a409bd
aza <nguyenhai011101@gmail.com>
2024-01-26 15:21:00 +0700

    add store - pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,7 @@ export default defineNuxtConfig({
 		},
 	},
 	modules: [
+		'@pinia/nuxt',
 		'@vueuse/motion/nuxt',
 		(_options, nuxt) => {
 			nuxt.hooks.hook('vite:extendConfig', (config) => {
