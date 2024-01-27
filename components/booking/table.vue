b3cd7b4695c69d928006fe542d9f9215bf51c598
aza <nguyenhai011101@gmail.com>
2024-01-27 19:59:13 +0700

    update custom input

---
+++ components/booking/table.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="flex items-center gap-5">
+		<CustomTextField
+			label="Person"
+			type="number"
+			:is-int="true"
+			:no-negative="true"
+			v-model:model-value="bodyBooking.persons"
+		/>
+
+		<CustomTextField
+			label="Person"
+			type="number"
+			:is-int="true"
+			:no-negative="true"
+			v-model:model-value="bodyBooking.persons"
+		/>
+
+		<CustomTextField
+			label="Person"
+			type="number"
+			:is-int="true"
+			:no-negative="true"
+			v-model:model-value="bodyBooking.persons"
+		/>
+	</div>
+</template>
+
+<script setup lang="ts">
+const bodyBooking = reactive({
+	persons: null,
+	date: null,
+	time: null,
+})
+</script>
+
+<style scoped></style>
